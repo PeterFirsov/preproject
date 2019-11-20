0c5ca28bf35412a6111012fdeea28c3956b630d4
PeterFirsov <pvfirsov15@yandex.ru>
2019-11-21 01:02:12 +0300

    сделал планшет-версию

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,67 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var open = document.querySelector('.header-top__open');
+var popup = document.querySelector('.write-us');
+var close = document.querySelector('.write-us__close');
 
-pageHeader.classList.remove('page-header--nojs');
+var questForm = document.querySelector('.question-form__form');
+var name = questForm.querySelector('[name=name]');
+var tel = questForm.querySelector('[name=tel]');
+var question = questForm.querySelector('[name=question]');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+var isStorageSupport = true;
+var storage = '';
+
+try {
+  storage = localStorage.getItem('name');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+open.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('write-us--show');
+});
+
+close.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.remove('write-us--show');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains('write-us--show')) {
+      evt.preventDefault();
+      popup.classList.remove('write-us--show');
+    }
+  }
+});
+
+(function (evt) {
+  evt.matches = evt.matches || evt.mozMatchesSelector || evt.msMatchesSelector || evt.oMatchesSelector || evt.webkitMatchesSelector;
+  evt.closest = evt.closest || function closest(selector) {
+    if (!this) return null;
+    if (this.matches(selector)) return this;
+    if (!this.parentElement) {
+      return null
+    } else return this.parentElement.closest(selector)
+  };
+}(Element.prototype));
+
+popup.addEventListener('mouseup', function (evt) {
+  if (evt.target.closest('.write-us__wrapper') === null) {
+    popup.classList.remove('write-us--show');
+  }
+});
+
+
+questForm.addEventListener('submit', function (evt) {
+  if (!name.value || !tel.value || !question.value) {
+    evt.preventDefault();
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    if (isStorageSupport) {
+      localStorage.setItem('name', name.value);
+      localStorage.setItem('tel', tel.value);
+      localStorage.setItem('question', question.value);
+    }
   }
 });
--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ gulp.task("server", function () {
     ui: false
   });
 
+  gulp.watch("source/js/**.{js}", gulp.series("copy"));
   gulp.watch("source/img/**.{png,jpg,svg}", gulp.series("copy"));
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
